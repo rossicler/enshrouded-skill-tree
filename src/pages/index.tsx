4ee9dff960a8c6b069a4131fabe4f23e2de6e42e
Rossicler Junior <rossiclerjr@gmail.com>
2024-02-22 17:38:37 -0500

    Added bg image optimization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,21 @@
 import SkillTree from "@/components/SkillTree";
 import { classNames } from "@/utils/utils";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main
       className={classNames(
         "h-screen w-screen max-h-screen max-w-full relative bg-gray-600 flex",
-        "items-center justify-center text-white wrapper bg-cover relative"
+        "items-center justify-center text-white"
       )}
-      style={{
-        backgroundImage: `url(./assets/Enshrouded_Skill_Tree_BG.png)`,
-      }}
     >
+      <Image
+        src="/assets/Enshrouded_Skill_Tree_BG.png"
+        alt="Purple smoke background"
+        fill
+        className="object-cover object-center"
+      />
       <div>
         <SkillTree />
       </div>

b77390231d49afa2be6f8db274db3243ab4189ca
Rossicler Junior <rossiclerjr@gmail.com>
2024-02-24 01:45:14 -0500

    Remove debug code

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,27 +1,24 @@
 import Script from "next/script";
 
-const GoogleAnalytics = ({ ga_id }: { ga_id: string }) => {
-  console.log(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
-  return (
-    <>
-      <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js? 
+const GoogleAnalytics = ({ ga_id }: { ga_id: string }) => (
+  <>
+    <Script
+      async
+      src={`https://www.googletagmanager.com/gtag/js? 
       id=${ga_id}`}
-      ></Script>
-      <Script
-        id="google-analytics"
-        dangerouslySetInnerHTML={{
-          __html: `
+    ></Script>
+    <Script
+      id="google-analytics"
+      dangerouslySetInnerHTML={{
+        __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', '${ga_id}');
         `,
-        }}
-      ></Script>
-    </>
-  );
-};
+      }}
+    ></Script>
+  </>
+);
 export default GoogleAnalytics;

431044e1212b914c4f3ceb8be311a736994aab26
Rossicler <rossiclerjr@gmail.com>
2025-08-26 07:24:16 -0400

    Added ads setup on page header

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -11,6 +11,11 @@ const CustomHeader = () => {
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1232366016476149"
+        crossOrigin="anonymous"
+      ></script>
       {/* <meta property="og:url" content={baseUrl} /> */}
     </Head>
   );
